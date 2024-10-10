de5036aefaaecafda1984f5d83517e473f9cc7f1
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-10-10 14:39:40 -0500

    refactor(pipe): monthYearPipe improves readability with dateHelper

--- src/app/core/pipe/month-year.pipe.ts
+++ src/app/core/pipe/month-year.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DateHelper } from '../index-util';
 
 @Pipe({
   name: 'monthYear'
@@ -6,41 +7,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class MonthYearPipe implements PipeTransform {
 
   transform(value: Date): string {
-    const items = value.toString().split(' ').slice(0, 4);
-    const year = items[3];
-    const month = this.completeMonth(items[1]);
+    const dataHelper: DateHelper = new DateHelper(value);
+    const year = value.getFullYear();
+    const month = dataHelper.getMonthName();
+    
     return `${month} ${year}`;
-  }
-
-  private completeMonth(month: string): string {
-    switch (month) {
-      case 'Jan':
-        return 'January';
-      case 'Feb':
-        return 'February';
-      case 'Mar':
-        return 'March';
-      case 'Apr':
-        return 'April';
-      case 'May':
-        return 'May';
-      case 'Jun':
-        return 'June';
-      case 'Jul':
-        return 'July';
-      case 'Aug':
-        return 'August';
-      case 'Sep':
-        return 'September';
-      case 'Oct':
-        return 'October';
-      case 'Nov':
-        return 'November';
-      case 'Dec':
-        return 'December';
-      default:
-        return month;
-    }
   }
 
 }
