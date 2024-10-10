ec8d8514715b0dab95f639ed1a6074dd7b33d41c
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-10-10 14:45:24 -0500

    feat(day-container): contents of the day view

--- src/app/feature/day-container/day-container.component.ts
+++ src/app/feature/day-container/day-container.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, Renderer2, ViewChild, ViewContainerRef } from '@angular/core';
+
+import { YearContainerComponent } from '../year-container/year-container.component';
+import { ModalService, UrlDateService } from '../../core/index-service';
+import { ModalConfig } from '../../core/index-model';
+import { Subscription } from 'rxjs';
+import { DateHelper } from '../../core/index-util';
 
 @Component({
   selector: 'app-day-container',
@@ -7,6 +13,65 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
 })
-export class DayContainerComponent {
+export class DayContainerComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('ContentDay') contentDayElementRef!: any;
+  @ViewChild('ContentEvent') contentEventElementRef!: any;
+  dateHelper: DateHelper = new DateHelper(new Date());
+  customDate: Date = new Date();
+  currentDate = new Date();
+  hours: string[] = [];
+  urlDateSub: Subscription = new Subscription();
 
+  constructor(
+    private modalSrv: ModalService,
+    private viewContainerRef: ViewContainerRef,
+    private render: Renderer2,
+    private urlDateSrv: UrlDateService,
+  ) {
+    this.createHours();
+  }
+
+  ngOnInit(): void {
+    this.urlDateSub = this.urlDateSrv.getDateFromUrlObservable().subscribe(date => {
+      this.customDate = date;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.calculateHeightContentEvent();
+  }
+
+  ngOnDestroy(): void {
+    if(this.urlDateSub) this.urlDateSub.unsubscribe();
+  }
+
+  private createHours() {
+    for(let i = 1; i <= 24; i++) {
+      let hourString = i.toString().padStart(2,'0') + ":00";
+      this.hours.push(hourString);
+    }
+  }
+
+  private calculateHeightContentEvent() {
+    if(!this.contentDayElementRef && !this.contentEventElementRef) return;
+    const heightHeaderCalendar = 59;
+    const heightContentDay = this.contentDayElementRef.nativeElement.offsetHeight;
+    const contentEventElement= this.contentEventElementRef.nativeElement;
+    this.render.setStyle(contentEventElement,'height',`calc(100vh - (${ heightContentDay + heightHeaderCalendar }px))`)
+  }
+
+  public modalOpen() {
+    const configureModal: ModalConfig = {
+      component: YearContainerComponent,
+      title: 'Add event'
+    }
+    this.modalSrv.openModal(this.viewContainerRef, configureModal);
+  }
+
+  public isCurrentDay(customDay: Date): boolean {
+    let current = this.dateHelper.getDateParts(this.currentDate)
+    let custom = this.dateHelper.getDateParts(customDay)
+
+    return current.year == custom.year && current.month == custom.month && current.day == custom.day;
+  }
 }
