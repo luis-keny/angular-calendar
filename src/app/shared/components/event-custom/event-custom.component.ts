047e6755d9d8cdf3ce00daa2766df5e642e9e13d
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-10-15 13:16:01 -0500

    feat(impl directive): directive used in the event customer

--- src/app/shared/components/event-custom/event-custom.component.ts
+++ src/app/shared/components/event-custom/event-custom.component.ts
@@ -1,19 +1,21 @@
-import { AfterViewInit, Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Task } from '../../../core/index-model';
+import { CalendarTaskDirective } from '../../../core/index-directive';
 
 @Component({
   selector: 'app-event-custom',
   templateUrl: './event-custom.component.html',
   styleUrl: './event-custom.component.css',
   standalone: true,
-  imports: [FormsModule]
+  imports: [FormsModule, CalendarTaskDirective],
 })
-export class EventCustomComponent implements OnInit, OnChanges {
+export class EventCustomComponent implements OnInit {
   @Input({ required: true }) heightPerHours!: number;
   @Input({ required: true }) unitOfMeasure!: string;
   @Input({ required: true }) tasks: Task[] = []
   @Input({ required: true }) dateSelected!: Date;
+
   currentMoment: Date = new Date();
 
   constructor() { }
@@ -24,35 +26,6 @@ export class EventCustomComponent implements OnInit, OnChanges {
     }, 60 * 1000);
   }
 
-  ngOnChanges(): void {
-    this.tasks.map(task => {
-      const { top, height } = this.getTopAndHeight(task.startTime, task.endTime);
-    
-      task.top = top
-      task.height = height;
-    })
-  }
-
-  private getTopAndHeight(startTime: string, endTime: string) {
-    const startTimeParts = startTime.split(':');
-    const endTimeParts = endTime.split(':');
-    
-    let startHour = parseInt(startTimeParts[0])
-    let startMinute = parseInt(startTimeParts[1])
-    
-    let endHour = parseInt(endTimeParts[0])
-    let endMinute = parseInt(endTimeParts[1])
-
-    const oneHour = this.heightPerHours;
-    const startTop = (oneHour*startHour + oneHour*(startMinute/60));
-    const endTop = (oneHour*endHour + oneHour*(endMinute/60));
-    
-    const top = startTop.toString() + this.unitOfMeasure;
-    const height = (endTop - startTop).toString() + this.unitOfMeasure;
-
-    return { top, height };
-  }
-
   public getTopOfCurrentMoment(): string {
     let hour = this.currentMoment.getHours();
     let minute = this.currentMoment.getMinutes();
@@ -60,7 +33,7 @@ export class EventCustomComponent implements OnInit, OnChanges {
     const oneHour = this.heightPerHours;
     const top = (oneHour*hour + oneHour*(minute/60));
 
-    return top + this.unitOfMeasure;
+    return `calc(${top + this.unitOfMeasure} - 0.4rem)`;
   }
 
   public isEqualDate(day1: Date, day2: Date): boolean {
@@ -69,22 +42,5 @@ export class EventCustomComponent implements OnInit, OnChanges {
     const isEqualDay = day1.getDate() == day2.getDate();
 
     return isEqualDay && isEqualMonth && isEqualYear;
-  }
-
-  public isLessThatCurrent(task: Task) {
-    const isLessYear = task.date.getFullYear() < this.currentMoment.getFullYear();
-    const isLessMonth = task.date.getMonth() < this.currentMoment.getMonth();
-    const isLessDay = task.date.getDate() < this.currentMoment.getDate();
-    const isEqualDay = task.date.getDate() == this.currentMoment.getDate();
-
-    const startTimeParts = task.startTime.split(':');
-    
-    let startHour = parseInt(startTimeParts[0])
-    let startMinute = parseInt(startTimeParts[1])
-
-    const isLessHour = startHour < this.currentMoment.getHours();
-    const isLessMinute = startMinute < this.currentMoment.getMinutes();
-
-    return isLessYear || isLessMonth || (isLessDay || (isLessHour && isLessMinute && isEqualDay));
   }
 }
