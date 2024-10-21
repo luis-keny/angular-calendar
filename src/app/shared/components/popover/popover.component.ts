960b40d287ec9b6f2e5414ed6c487938271d8fa6
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-10-20 20:17:39 -0500

    feat: popover for events

---
+++ src/app/shared/components/popover/popover.component.ts
@@ -0,0 +1,58 @@
+import { Component, ElementRef, Input, OnChanges, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
+
+import { TaskGroup } from '../../../core/index-model';
+import { TaskService } from '../../../core/index-service';
+
+@Component({
+  selector: 'app-popover',
+  templateUrl: './popover.component.html',
+  styleUrl: './popover.component.css',
+  standalone: true,
+  imports: [CommonModule],
+})
+export class PopoverComponent implements OnChanges, OnDestroy {
+  @Input({ required: true }) date!: Date;
+  @Input({ required: true }) childElement!: ElementRef;
+  @Input({ required: true }) parentElement!: ElementRef;
+
+  group: TaskGroup = { date: new Date(), tasks: [] };
+  taskSub: Subscription = new Subscription();
+  
+  top: string = '1rem';
+  left: string = '5rem';
+  width = '180px';
+
+  constructor(
+    private taskSrv: TaskService,
+  ) { }
+
+  ngOnChanges(): void {
+    this.taskSub = this.taskSrv.getTaskOfDay(this.date).subscribe(group => this.group = group);
+    this.definePosition();
+  }
+
+  ngOnDestroy(): void {
+    if(this.taskSub) this.taskSub.unsubscribe();
+  }
+
+  public definePosition(){
+    this.defineCenterPosition();
+  }
+
+  private defineCenterPosition() {
+    if(this.childElement === undefined) {
+      this.top = "-999px";
+      this.left = "-999px";
+      return;
+    }
+    
+    const childElem = this.childElement.nativeElement;
+    let width = (childElem.clientWidth - parseInt(this.width)) / 2;
+    let leftPosition = childElem.offsetLeft + width > 0 ? childElem.offsetLeft + width : 0;
+    
+    this.top = childElem.offsetTop + "px";
+    this.left = leftPosition + "px";
+  }
+}
